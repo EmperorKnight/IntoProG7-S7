a0c7206c58ddd214a73bddf4a2afe9c8385894ea
EmperorKnight <jlpicado@uamv.edu.ni>
2025-05-05 14:33:48 -0600

    Por fin

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
-    msn = ""
-    for num in range(1,11):
-        msn += str(num)
-    return render_template("index.html", msn = msn)
+    return render_template("index.html")
+
+@app.route("/Elegir")
+
+def Ejercicio_1():
+    return render_template("Ejercicio_1.html")
+
+@app.route("/Sumar", methods = ["post"])
+
+def sumar():
+    numero = 0
+    N = request.form.get("N")
+    N = int(N)
+    for i in range(0+1,N+1):
+        numero += i
+    resultado = f"{numero:,}"
+    return render_template("Ejercicio_1.html", resultado = resultado)
 
 if __name__ == "__main__":
     app.run(debug = True)
-
